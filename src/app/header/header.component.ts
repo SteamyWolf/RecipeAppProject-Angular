521f8ca7ef0b9f583590c3dac45477fbb889fc1f
Wyatt Allan <wyatthunterallan@gmail.com>
2020-06-15 12:15:00 -0600

    Added interceptor and new fetchng data with authentication

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,17 +1,25 @@
-import { Component, OnInit} from '@angular/core';
+import { Component, OnInit, OnDestroy} from '@angular/core';
 import { DataStorageService } from '../shared/data-storage.service';
+import { AuthService } from '../auth/auth.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, OnDestroy {
+  isAuthenticated = false;
+  private userSub: Subscription;
 
-
-  constructor(private dataStorageService: DataStorageService) { }
+  constructor(private dataStorageService: DataStorageService, private authService: AuthService) { }
 
   ngOnInit(): void {
+    this.userSub = this.authService.user.subscribe(user => {
+      this.isAuthenticated = !!user;
+      console.log(!user)
+      console.log(!!user)
+    })
   }
 
   onSaveData() {
@@ -22,4 +30,7 @@ export class HeaderComponent implements OnInit {
     this.dataStorageService.fetchRecipes().subscribe()
   }
 
+  ngOnDestroy() {
+    this.userSub.unsubscribe();
+  }
 }
